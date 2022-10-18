bf5dd0259b8a05e51165e274e6fb3bc90e82bafd
EvelynOvi <oviangbedeevelyn@gmail.com>
2022-10-17 21:03:16 -0700

    update

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
 #include <stdarg.h>
 
 
@@ -42,4 +44,39 @@ typedef struct flags_printf
 	char *c;
 	int (*f)(va_list);
 } flags_p;
+
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+			int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+			int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+			int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+			int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+		char buffer[],
+			int flags, int width, int precision, int size);
+
+
 #endif
