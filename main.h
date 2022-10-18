4f4909bd1ab7b2094d92b97bf4252f96ab8828f5
bigboytimi <olutimiolowookere@icloud.com>
2022-10-18 06:38:07 +0000

    hot fix

--- main.h
+++ main.h
@@ -45,23 +45,4 @@ typedef struct flags_printf
 	int (*f)(va_list);
 } flags_p;
 
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
-
-/* Funciotns to handle other specifiers */
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-
-
 #endif
