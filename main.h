2a80f3bf92c5486340cf3a80c42dbac7d4f01b9c
EvelynOvi <oviangbedeevelyn@gmail.com>
2022-10-17 21:24:02 -0700

    quick fix

--- main.h
+++ main.h
@@ -64,19 +64,4 @@ int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
 
-/* width handler */
-int handle_write_char(char c, char buffer[],
-			int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[],
-			int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-			int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length,
-			int width, int flags, char padd, char extra_c, int padd_start);
-
-int write_unsgnd(int is_negative, int ind,
-		char buffer[],
-			int flags, int width, int precision, int size);
-
-
 #endif
